6fce090819cacddb5bf3338bca2d65f5bc370cbe
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-07-23 15:59:49 +0530

    learn react create element

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -3,11 +3,20 @@ import Chai from "./chai"
 
 
 function App() {
+
+  const username = "chai aur code"
  
 
   return (
+    <>
    <Chai/>
+   <h1>chai aur react {username}</h1>
+   <p>test para</p>
+   </>
   )
 }
 
 export default App
+
+
+//   '${}' treated like variable in js  same as '{}' treate variable in react its also called expression or we can say evalueated expression
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+         // this code not execute become the syntax is not upto react object classification
+// const reactElement = {
+//   type: 'a',                         
+//   props: {
+//       href: 'https://google.com',
+//       traget: '_blank'
+//   },
+//   Children: 'Click me to visit google'
+// }
+
+      // this code executed
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+     const anotherUser = "chai aur react"
+
+   const reactElement = React.createElement(   // by deafult injector (babel injection) transpil the jsx code into simple react functions the can be understood by browsers
+         'a',   // html element
+         {href: 'https://google.com', target: '_blank'},  // set Attribute for element if dont have any set empty it
+         'click me to visit google',  // text to inject in html tag
+           anotherUser   // variable injection after tree is fromed (evalueted expression)
+   )
+
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // reactElement
+    //anotherElement
+
+    reactElement
+
+    // <App />
+
 )
